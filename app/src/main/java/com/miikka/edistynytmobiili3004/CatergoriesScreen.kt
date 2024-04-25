0b6e61f80d51ad12c06ab967032a6f98ba516a20
MiikkaRautio <2108027@edu.karelia.fi>
2024-04-25 22:04:07 +0300

    Adding Categories

--- app/src/main/java/com/miikka/edistynytmobiili3004/CatergoriesScreen.kt
+++ app/src/main/java/com/miikka/edistynytmobiili3004/CatergoriesScreen.kt
@@ -1,10 +1,18 @@
 package com.miikka.edistynytmobiili3004
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -12,11 +20,18 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.miikka.edistynytmobiili3004.viewmodel.CategoriesViewModel
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CategoriesScreen(onMenuClick: () -> Unit){
+    val categoriesVm: CategoriesViewModel = viewModel()
     Scaffold(topBar = {
         TopAppBar(title = { Text(text = "Categories") }, navigationIcon = {
             IconButton(onClick = { onMenuClick() }) {
@@ -24,6 +39,33 @@ fun CategoriesScreen(onMenuClick: () -> Unit){
             }
         })
     }) {
-        LazyColumn(modifier = Modifier.padding(it)){}
+        paddingValues ->
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)){
+            when{
+                categoriesVm.categoriesState.value.loading -> CircularProgressIndicator(
+                    modifier = Modifier.align(
+                        Alignment.Center
+                    )
+                )
+                else -> LazyColumn(){
+                    items(categoriesVm.categoriesState.value.list){
+                        Column(modifier = Modifier.fillMaxWidth()) {
+                            Row(modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp),
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ){
+                                Text("Kuva")
+                                Column(horizontalAlignment = Alignment.End) {
+                                    Text(text = it.name, fontWeight = FontWeight.ExtraBold)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/miikka/edistynytmobiili3004/viewmodel/CategoriesViewModel.kt
+++ app/src/main/java/com/miikka/edistynytmobiili3004/viewmodel/CategoriesViewModel.kt
@@ -1,7 +1,41 @@
 package com.miikka.edistynytmobiili3004.viewmodel
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.miikka.edistynytmobiili3004.model.CategoriesState
+import com.miikka.edistynytmobiili3004.model.CategoryItem
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class CategoriesViewModel: ViewModel() {
+
+
+
+    private val _categoriesState = mutableStateOf(CategoriesState())
+    val categoriesState: State<CategoriesState> = _categoriesState
+
+    init {
+        getCategories()
+    }
+
+    private suspend fun waitForCategories(){
+        delay(2000)
+    }
+
+    fun getCategories(){
+        viewModelScope.launch {
+            _categoriesState.value = _categoriesState.value.copy(loading = true)
+            waitForCategories()
+            _categoriesState.value = categoriesState.value.copy(
+                loading = false,
+                list = listOf(
+                    CategoryItem(id=1, name="Kategoria1"),
+                    CategoryItem(id=2, name="Kategoria2")
+                )
+            )
+        }
+    }
 
 }
